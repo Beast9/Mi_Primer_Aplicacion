830dfd33bc7ed7d10c31d0894560768e3aa13c2a
Beast9 <deadbrain9@hotmail.com>
2016-05-02 13:43:09 -0600

    Initial commit

---
+++ app/src/test/java/beast9/example/org/mi_primer_aplicacion/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package beast9.example.org.mi_primer_aplicacion;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
